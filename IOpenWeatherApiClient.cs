a5d0e6a473e4e403a687f867e1714a585ebd935d
Federico Teotini <teotini.federico@gmail.com>
2022-08-26 10:32:31 +0200

    Extracted IOpenWeatherApiClient.cs

---
+++ IOpenWeatherApiClient.cs
@@ -0,0 +1,11 @@
+using giuneco.wth;
+using Refit;
+
+public interface IOpenWeatherApiClient
+{
+    [Get("/weather?units=metric&lang=it")]
+    Task<CurrentWeatherResponse> GetCurrentWeatherAsync([AliasAs("appid")] string apiKey, [AliasAs("q")] string location);
+    
+    [Get("/forecast?units=metric&lang=it")]
+    Task<WeatherForecastResponse> GetWeatherForecastAsync([AliasAs("appid")] string apiKey, [AliasAs("q")] string location);
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text.Json;
-using giuneco.wth;
 using Refit;
 
 var api = RestService.For<IOpenWeatherApiClient>("https://api.openweathermap.org/data/2.5");
@@ -9,13 +8,3 @@ var api = RestService.For<IOpenWeatherApiClient>("https://api.openweathermap.org
 var res = await api.GetWeatherForecastAsync(Environment.GetEnvironmentVariable("WEATHER_API"), "Piombino,it");
 
 Console.WriteLine(JsonSerializer.Serialize(res,new JsonSerializerOptions{WriteIndented = true}));
-
-public interface IOpenWeatherApiClient
-{
-    [Get("/weather?units=metric&lang=it")]
-    Task<CurrentWeatherResponse> GetCurrentWeatherAsync([AliasAs("appid")] string apiKey, [AliasAs("q")] string location);
-    
-    [Get("/forecast?units=metric&lang=it")]
-    Task<WeatherForecastResponse> GetWeatherForecastAsync([AliasAs("appid")] string apiKey, [AliasAs("q")] string location);
-}
-
